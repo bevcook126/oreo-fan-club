abd4c08281194f6a1b11f1077facac56c5264b0c
username <bevcook126@gmail.com>
2022-07-22 13:28:05 -0700

    rename routes, nav bar

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,6 +40,8 @@ app.use(function(req, res, next) {
   res.locals.user = req.user;
   next();
 });
+
+const isLoggedIn = require('./config/auth');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
