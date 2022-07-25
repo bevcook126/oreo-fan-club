be32057e286947c1fa2a6aa5190e30651a809f8e
username <bevcook126@gmail.com>
2022-07-25 16:28:59 -0700

    add country form

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,3 +1,14 @@
-const Country = require('../models/country');
 const Oreo = require('../models/oreo');
 
+module.exports = {
+    create,
+}
+
+function create(req, res) {
+    Oreo.findById(req.params.id, function(err, oreo) {
+        oreo.countries.push(req.body);
+        oreo.save(function(err) {
+            res.redirect(`/oreos/${oreo._id}`);
+        });
+    });
+}
--- routes/countries.js
+++ routes/countries.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const countriesCtrl = require('../controllers/countries');
 
-router.get('oreos/:id/countries/new', countriesCtrl.new);
 router.post('/oreos/:id/countries', countriesCtrl.create);
 
 module.exports = router;
