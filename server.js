bc75a88ffb55140ee938adbdf1069d8a995a7431
username <bevcook126@gmail.com>
2022-07-29 09:51:25 -0700

    favicon

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ var countriesRouter = require('./routes/countries');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -46,22 +45,17 @@ app.use(function(req, res, next) {
 const isLoggedIn = require('./config/auth');
 
 app.use('/', indexRouter);
-app.use('/', isLoggedIn, reviewsRouter);
+app.use('/', reviewsRouter);
 app.use('/oreos', isLoggedIn, oreosRouter);
 app.use('/countries', isLoggedIn, countriesRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
